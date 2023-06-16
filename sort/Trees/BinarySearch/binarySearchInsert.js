6d3d4add4c52bc9782ca9172e99ef84d86ca4b2f
Karthik G S <karthikgs005@gmail.com>
2023-06-16 18:57:29 +0530

    Add initial files

---
+++ sort/Trees/BinarySearch/binarySearchInsert.js
@@ -0,0 +1,22 @@
+
+
+class Node {
+    constructor() {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchInsert {
+    constructor() {
+        this.root = null
+    }
+
+    isEmpty () {
+        return this.root = null
+    }
+}
+
+
+
