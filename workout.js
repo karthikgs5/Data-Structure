a428b47a9fdfc6ea63fee9d2b2e6142ce25ccca9
Karthik G S <karthikgs005@gmail.com>
2023-07-01 11:41:11 +0530

    new

--- workout.js
+++ workout.js
@@ -82,3 +82,53 @@ bst.insert(30)
 console.log(bst.isEmpty())
 console.log(bst.search(10,bst.root))
 bst.inOrder(bst.root)
+
+
+
+//GRAPH .......................\
+class Graph {
+  constructor() {
+      this.adjacencyList ={}
+  }
+
+  addVertex(vertex) {
+      if(!this.adjacencyList[vertex]) {
+          this.adjacencyList[vertex] = new Set()
+      }
+  }
+
+  addEdge(vertex1 , vertex2) {
+      if(!this.adjacencyList[vertex1]){
+          this.addVertex(vertex1)
+      }
+      if(!this.adjacencyList[vertex2]){
+          this.addVertex(vertex2)
+      }
+      this.adjacencyList[vertex1].add(vertex2)
+      this.adjacencyList[vertex2].add(vertex1)
+  }
+
+  hasEdge(vertex1 , vertex2) {
+      return (
+          this.adjacencyList[vertex1].has(vertex2) &&
+          this.adjacencyList[vertex2].has(vertex1)
+      )
+  }
+
+  display() {
+      for(let vertex in this.adjacencyList){
+          console.log(vertex +"->" + [...this.adjacencyList[vertex]]);
+      }
+  }
+}
+
+const graph = new Graph()
+
+graph.addVertex("A")
+graph.addVertex("B")
+graph.addVertex("C")
+
+graph.addEdge("A" ,"B")
+graph.addEdge("B" ,"C")
+graph.display()
+console.log(graph.hasEdge("A","B"));
